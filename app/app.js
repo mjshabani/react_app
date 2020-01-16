a701b96b985e815ed1f1a9a98082976ca7483b79
MJavad <mjshz.73@gmail.com>
2020-01-16 19:19:08 +0330

    some changes

--- app/app.js
+++ app/app.js
@@ -1,9 +1,29 @@
 var express = require("express");
 var path = require("path");
- 
-var app = express();
-app.use(express.static(path.join(__dirname,"/html")));
+var bodyParser = require("body-parser");
 
-app.listen(8000,function(){
-    console.log("Started listening on port", 8000);
+var app = express();
+app.use(express.static(path.join(__dirname, "/html")));
+app.use(bodyParser.json());
+
+app.post('/signin', function (req, res) {
+  var user_name = req.body.email;
+  var password = req.body.password;
+  console.log("here")
+  console.log(user_name)
+  console.log(password)
+  if (user_name == 'admin' && password == 'admin') {
+    res.send('success');
+  }
+  else {
+    res.send('Failure');
+  }
 })
+
+app.listen(8000, function () {
+  console.log("Started listening on port", 8000);
+})
+
+axios.defaults.baseURL = 'http://myurl';
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
